16967b5522d2b2007da83e73d5491e84cbd0ebe1
Afefferhichi <afef.ferchichi95@gmail.com>
2020-08-27 21:27:38 +0200

    first commit of 3almadmoon backend project

---
+++ model/user.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    address : {
+        type: String,
+        required: true
+    },
+    telephone : {
+        type: Number,
+        required: true
+    },
+    organisation: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String
+    },
+    photoLocation: {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
+
+});
+//,{ collection: 'users' });
+
+module.exports = mongoose.model('User', userSchema);
